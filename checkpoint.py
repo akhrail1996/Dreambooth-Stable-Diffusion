4bf3453c853c657ea962303e66d6a094a1f0009a
Rail Akhmetov <akhrail1996@gmail.com>
2023-03-04 21:14:37 +0400

    Update checkpoint.py

--- checkpoint.py
+++ checkpoint.py
@@ -1,9 +1,15 @@
+import subprocess
 import re
 
-training_images = !find training_images/*
-date_string = !date +"%Y-%m-%dT%H-%M-%S"
+training_images_output = subprocess.run(["find", "training_images/*"], capture_output=True, text=True)
+training_images = training_images_output.stdout.strip().split("\n")
 
-directory_paths = !ls -d logs/*
+date_string_output = subprocess.run(["date", "+%Y-%m-%dT%H-%M-%S"], capture_output=True, text=True)
+date_string = date_string_output.stdout.strip().split("\n")
+
+directory_paths_output = subprocess.run(["ls", "-d", "logs/*"], capture_output=True, text=True)
+directory_paths = directory_paths_output.stdout.strip().split("\n")
+
 last_checkpoint_file = directory_paths[-1] + "/checkpoints/last.ckpt"
 file_name = date_string[-1] + "_" + \
             str(len(training_images)) + "_training_images_" + \
@@ -13,7 +19,7 @@ file_name = date_string[-1] + "_" + \
 
 file_name = file_name.replace(" ", "_")
 
-!mkdir -p trained_models
-!mv "{last_checkpoint_file}" "trained_models/model.ckpt"
+subprocess.run(["mkdir", "-p", "trained_models"])
+subprocess.run(["mv", last_checkpoint_file, "trained_models/model.ckpt"])
 
 print("Download your trained model from trained_models/" + file_name + " and use in your favorite Stable Diffusion repo!")
