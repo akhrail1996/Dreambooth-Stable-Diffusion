d05a3df5b7848df542fa31860bdda475315c505c
Rail Akhmetov <akhrail1996@gmail.com>
2023-03-04 16:22:45 +0400

    Update everydream.py

--- everydream.py
+++ everydream.py
@@ -4,7 +4,6 @@ downloaded_model_path = hf_hub_download(repo_id="panopstor/EveryDream", filename
 print(downloaded_model_path)
 
 # Move the sd_v1-5_vae.ckpt to the root of this directory as "model.ckpt"
-# actual_locations_of_model_blob = !readlink -f {downloaded_model_path}
-# !mv {actual_locations_of_model_blob[-1]} model.ckpt
+!mv downloaded_model_path model.ckpt
 
-# print("✅ model.ckpt successfully downloaded")
+print("✅ model.ckpt successfully downloaded")
