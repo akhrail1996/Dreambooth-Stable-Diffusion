9f82aa6a50cb5608a9fb619aae6d07a2144822a1
Rail Akhmetov <akhrail1996@gmail.com>
2023-03-04 16:36:48 +0400

    Update everydream.py

--- everydream.py
+++ everydream.py
@@ -4,6 +4,8 @@ downloaded_model_path = hf_hub_download(repo_id="panopstor/EveryDream", filename
 print(downloaded_model_path)
 
 # Move the sd_v1-5_vae.ckpt to the root of this directory as "model.ckpt"
-!mv downloaded_model_path model.ckpt
+import subprocess
+out = subprocess.run("mv " + downloaded_model_path + " model.ckpt", shell=True, stdout=subprocess.PIPE)
+print(out.stdout)
 
 print("✅ model.ckpt successfully downloaded")
