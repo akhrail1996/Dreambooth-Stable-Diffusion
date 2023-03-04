9efd8993ad1d3b9dec824deb1046b4d9d6f497d2
Rail Akhmetov <akhrail1996@gmail.com>
2023-03-04 17:03:20 +0400

    Update everydream.py

--- everydream.py
+++ everydream.py
@@ -6,7 +6,7 @@ print(downloaded_model_path)
 # Move the sd_v1-5_vae.ckpt to the root of this directory as "model.ckpt"
 import subprocess
 
-actual_locations_of_model_blob = subprocess.run(!readlink -f {downloaded_model_path}, shell=True, stdout=subprocess.PIPE).stdout
+actual_locations_of_model_blob = subprocess.run("readlink -f " + downloaded_model_path, shell=True, stdout=subprocess.PIPE).stdout
 print(actual_locations_of_model_blob)
 
 out = subprocess.run("mv " + actual_locations_of_model_blob[-1] + " model.ckpt", shell=True, stdout=subprocess.PIPE)
